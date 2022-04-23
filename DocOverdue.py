4c94ac19735ba859bfd6524c989037b27e04abb1
Nklobe <tobbe_chevy@hotmail.com>
2022-04-23 13:23:16 +0200

    First of the day push

--- DocOverdue.py
+++ DocOverdue.py
@@ -23,12 +23,18 @@ def run_command(cmd):  # runs command and trims the output
     for c in range(len(outCMD)):
         outCMD[c] = outCMD[c].decode('utf-8')
         outCMD[c] = outCMD[c].replace('\'', '')
-        #print(outCMD[c])
     return outCMD
 
 
-def fetch_installed_packages():
-    # gets all the install packages
+def parse_output(byteList):
+    byteList = byteList.stdout.splitlines()
+    for c in range(len(byteList)):
+        byteList[c] = byteList[c].decode('utf-8')
+        byteList[c] = byteList[c].replace('\'', '')
+    return byteList
+
+
+def fetch_installed_packages():  # gets all installed packages via dpkg
     print_sign("Fetching installed packages")
     cmd = ["dpkg-query", "-f", "'${Package}\n'", "-W"]
     applicationList = run_command(cmd)
@@ -37,7 +43,7 @@ def fetch_installed_packages():
     return applicationList
 
 
-def fetch_package_files(packageList):
+def fetch_package_files(packageList):  # checks files related to package
     print_sign("Fetching package files")
     fileList = {}
     for p in packageList:
@@ -45,6 +51,41 @@ def fetch_package_files(packageList):
         fileList[p] = run_command(cmd)
         sys.stdout.write("Package scanned: ")
         print(p)
+    return fileList
+
+
+def parse_config_files(fileDict):  # checks for files located in /etc
+
+    print_sign("Parsing config files")
+
+    for f in fileDict.items():  # gets through the items
+        Addcolon = f[0] + ":"
+        etcFiles = {}
+        fileList = []
+        for fileURL in f[1]:  # looping through the file lis
+            if "/etc/" in fileURL and Addcolon in fileURL:
+                print(fileURL)
+                fileList.append(fileURL)
+            etcFiles[f[0]] = fileList
+        return etcFiles
+
+    # Lägga in någon form av regex som plockar ut alla delar av det
+
+
+def find_references():
+    # jag ska hitta en bra referens av filerna.
+
+    pass
+
+
+def download_package(packageList):
+    # download package
+    pass
+
+
+def extract_files():
+    # extract config files
+    pass
 
 
 def print_sign(label):  # for making pretty signs
@@ -53,8 +94,34 @@ def print_sign(label):  # for making pretty signs
     print("#" * 40)
 
 
+
+
 installedPackages = fetch_installed_packages()
-applicationFiles = fetch_package_files(installedPackages)
+#applicationFiles = fetch_package_files(installedPackages)
+applicationFiles = fetch_package_files(["apt"])
+#print(applicationFiles)
+etcFiles = parse_config_files(applicationFiles)
+for x in etcFiles["apt"]:
+    print(x)
+#print(etcFiles)
+
+#print(applicationFiles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # for x in applicationList:
